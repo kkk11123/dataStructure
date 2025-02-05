b5d375cd2144af6783f3b3cf45004f2d709159f9
kkk11123 <kang001016@naver.com>
2025-02-05 18:41:41 +0900

    .

--- SSL.c
+++ SSL.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
+#include <stdlib.h>
 
-int main()
-{
+
 	typedef int ElementType;
 	
 	typedef struct tagNote
@@ -28,27 +28,27 @@ int main()
 	  	free(Node);
 	  }
 	  
-	  void SLL_AppendNode(Node** Head, Node* NewNode)
-	  {
-	  	
-	  	// 헤드 노드가 NULL이라면 새로운 노드가 Head가 된다.
-		  if( (*Head) == NULL)
-		  {
-		  	*Head = NewNode;
-		   } 
-		   else
-		   {
-		   	// 테일을 찾아 NewNode를 연결한다.
-			   Node* Tail = (*Head) ;
-			   while (Tail -> NextNode != NULL)
-			   {
-			   	Tail = Tail->NextNode;
-			   }
-		   }
-	  }
-	  Node* List = NuLL;
+	  // Node 추가
+        void SLL_AppendNode(Node** Head, Node* NewNode) {
+            if (*Head == NULL) {
+                *Head = NewNode; // 헤드가 비어있다면 새 노드가 헤드가 됨
+            } else {
+                // 마지막 노드를 찾아 NewNode를 연결
+                Node* Tail = *Head;
+                while (Tail->NextNode != NULL) {
+                    Tail = Tail->NextNode;
+                }
+                Tail->NextNode = NewNode; // 마지막 노드의 NextNode를 새 노드로 연결
+            }
+        }
+
+    int main()
+    {
+	  Node* List = NULL;
 	  Node* NewNode = NULL;
-	  NewNode = SLL_CreateNode
-	 
-	return 0;
-}
+	  NewNode = SLL_CreateNode(117); //자유 저장소에 노드 생성
+      SLL_AppendNode(&List, NewNode); //생성한 노드를 List에 추가
+      NewNode = SLL_CreateNode(119); //자유 저장소에 또 다른 노드 생성
+      SLL_AppendNode(&List, NewNode); //생성한 노드를 List에 추가
+	  
+    }
